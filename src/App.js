46007b24af03e74ee9ef02383470475575548121
Gokulnath T <gokulnath.thirukkamu@softsuave.com>
2023-05-31 17:06:23 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h4 className="Desc">My web app is under development</h4>
-        <h5 className="Desc">This update is deployed from github</h5>
+        <h5 className="Desc">second update from github</h5>
       </header>
     </div>
   );
