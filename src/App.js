0353ec88a5fe640d689e64502d96c2792ef9130f
Gokulnath T <gokulnath.thirukkamu@softsuave.com>
2023-05-31 17:01:17 +0530

    initialize

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4 className="Desc">My web app is under development</h4>
+        <h5 className="Desc">This update is deployed from github</h5>
       </header>
     </div>
   );
